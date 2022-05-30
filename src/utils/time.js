090d8bdc1a637acf5b524edae3ec0970d915bdbc
Sergey <sergey.rubnikovich@gmail.com>
2022-05-30 13:34:21 +0700

    Del /utils

--- src/utils/time.js
+++
@@ -1,6 +0,0 @@
-export function getDateAsHeader(date) {
-    /*
-    * Return format-date. Example: `13, January`
-    * */
-    return date.getDate() + ", " + date.toLocaleString("default", {month: "long"});
-}
