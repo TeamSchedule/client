d916132be63f4cbcf91fe089140a5d4a8df8067d
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-01-22 04:37:33 +0700

    Unfixed header

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import { Outlet, useLoaderData } from "react-router-dom";
 
 import Header from "./header";
-import { HEADER_HEIGHT } from "../consts";
 
 export default function App() {
     const user: any = useLoaderData();
@@ -9,7 +8,7 @@ export default function App() {
     return (
         <div className="h-100 container-fluid m-0 p-0">
             <Header user={user} />
-            <div className="position-relative" style={{ top: `${HEADER_HEIGHT + 10}px` }}>
+            <div className="position-relative">
                 <div className="row m-0 d-flex px-2">
                     <div className="col-sm-12 p-0 position-relative">
                         <Outlet />
--- src/consts/common.ts
+++ src/consts/common.ts
@@ -7,7 +7,4 @@ const MIN_PASSWORD_LENGTH: number = 6;
 // минимальная длина названия команды
 const MIN_TEAM_NAME_LEN: number = 4;
 
-// высота хеадера в пикселях
-const HEADER_HEIGHT: number = 60;
-
-export { HEADER_HEIGHT, MIN_PASSWORD_LENGTH, MIN_TEAM_NAME_LEN, MIN_USERNAME_LENGTH };
+export { MIN_PASSWORD_LENGTH, MIN_TEAM_NAME_LEN, MIN_USERNAME_LENGTH };
