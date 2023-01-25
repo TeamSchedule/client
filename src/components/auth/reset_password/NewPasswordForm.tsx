2675994360cbea89fcabf312639163063b40eefc
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-01-26 01:55:45 +0700

    NewPasswordForm

--- src/components/auth/reset_password/NewPasswordForm.tsx
+++ src/components/auth/reset_password/NewPasswordForm.tsx
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import AuthFormLayout from "../AuthFormLayout";
 import BaseButton from "../../buttons/BaseButton";
-import React, { FormEvent, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import { CreateNewPasswordRequestSchema } from "../../../schemas/requests/auth";
 import { API } from "../../../api/api";
+import { ERRORS, MIN_PASSWORD_LENGTH } from "../../../consts";
+import { PasswordInput } from "../../inputs";
+import styles from "../Auth.module.scss";
+import ErrorMsg from "../../ErrorMsg";
 
 export default function NewPasswordForm() {
     /*
@@ -11,7 +15,43 @@ export default function NewPasswordForm() {
      * */
     const navigate = useNavigate();
 
+    const [password, setPassword] = useState<string | undefined>();
+    const [password2, setPassword2] = useState<string | undefined>();
+    const [isPasswordsMatch, setIsPasswordsMatch] = useState<boolean | undefined>();
+    const [isPasswordHasGoodLen, setIsPasswordHasGoodLen] = useState<boolean | undefined>();
+    const [isPasswordsOK, setIsPasswordsOK] = useState<boolean | undefined>();
+
     const [isActionInProgress, setIsActionInProgress] = useState(false);
+    const [isNewPasswordErrShown, setIsNewPasswordErrShown] = useState(false);
+
+    useEffect(() => {
+        // Если пароли еще не изменялись
+        if (password === undefined && password2 === undefined) return;
+
+        // Если пароль короткий
+        if (password && password.length < MIN_PASSWORD_LENGTH) {
+            setIsPasswordHasGoodLen(false);
+            return;
+        }
+        setIsPasswordHasGoodLen(true);
+
+        // пользователь еще не начал вводить второй пароль
+        if (password2 === undefined) return;
+
+        // Если пароли не совпадают
+        if (password !== password2) {
+            setIsPasswordsMatch(false);
+            return;
+        }
+        setIsPasswordsMatch(true);
+    }, [password, password2]);
+
+    useEffect(() => {
+        if (isPasswordHasGoodLen === undefined || isPasswordsMatch === undefined) {
+            setIsPasswordsOK(undefined);
+        }
+        setIsPasswordsOK(isPasswordHasGoodLen && isPasswordsMatch);
+    }, [isPasswordsMatch, isPasswordHasGoodLen]);
 
     function onInputNewPassword(event: FormEvent) {
         event.preventDefault();
@@ -22,8 +62,13 @@ export default function NewPasswordForm() {
 
         API.auth
             .createNewPassword(newPasswordData)
-            .then(() => {})
-            .catch(() => {})
+            .then(() => {
+                setIsNewPasswordErrShown(false);
+                navigate("");
+            })
+            .catch(() => {
+                setIsNewPasswordErrShown(true);
+            })
             .finally(() => {
                 setIsActionInProgress(false);
             });
@@ -33,10 +78,33 @@ export default function NewPasswordForm() {
         <>
             <AuthFormLayout onSubmit={onInputNewPassword}>
                 <>
+                    <PasswordInput
+                        value={password}
+                        setValue={setPassword}
+                        isOk={isPasswordHasGoodLen || isPasswordsOK}
+                        className={styles.formInputWrapper}
+                    />
+                    <PasswordInput
+                        value={password2}
+                        setValue={setPassword2}
+                        placeholder="Повторите пароль"
+                        isOk={isPasswordsOK}
+                        className={styles.formInputWrapper}
+                    />
+                    <ErrorMsg
+                        errText={ERRORS.SignUp.PasswordsDontMatch}
+                        visible={isPasswordsMatch !== undefined && !isPasswordsMatch}
+                    />
+                    <ErrorMsg
+                        errText={ERRORS.SignUp.PasswordsTooShort}
+                        visible={isPasswordHasGoodLen !== undefined && !isPasswordHasGoodLen}
+                    />
+                    <ErrorMsg errText={ERRORS.Service.ServiceUnavailable} visible={isNewPasswordErrShown} />
+
                     <BaseButton
-                        text="Сохрнаить пароль"
+                        text="Сохранить пароль"
                         color="success"
-                        disabled={true}
+                        disabled={!isPasswordsOK}
                         loading={isActionInProgress}
                         className="mt-3"
                     />
--- src/routes/UnauthorizedRoutes.tsx
+++ src/routes/UnauthorizedRoutes.tsx
@@ -8,6 +8,7 @@ import ForgotPasswordForm from "../components/auth/reset_password/ForgotPassword
 import {
     forgotPasswordPath,
     loginPath,
+    newPasswordPath,
     notFound,
     registrationPath,
     resetPasswordCodePath,
@@ -15,6 +16,7 @@ import {
     successRegistrationPath,
 } from "./paths";
 import ResetPasswordCodeForm from "../components/auth/reset_password/ResetPasswordCodeForm";
+import NewPasswordForm from "../components/auth/reset_password/NewPasswordForm";
 
 export const unauthorizedRouter = createBrowserRouter(
     createRoutesFromElements(
@@ -22,6 +24,7 @@ export const unauthorizedRouter = createBrowserRouter(
             <Route path={loginPath} element={<LoginForm />} />
             <Route path={forgotPasswordPath} element={<ForgotPasswordForm />} />
             <Route path={resetPasswordCodePath} element={<ResetPasswordCodeForm />} />
+            <Route path={newPasswordPath} element={<NewPasswordForm />} />
 
             <Route path={registrationPath} element={<RegisterForm />} />
             <Route path={successRegistrationPath} element={<SuccessRegisteredMsg />} />
