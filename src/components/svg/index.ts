7b2db6cd50509731d94194bee88d14ae9f409b41
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-03-10 16:54:14 +0700

    Drop unused icons

--- src/components/svg/index.ts
+++
@@ -1,6 +0,0 @@
-import AlternateAvatarImageIcon from "./AlternateAvatarImageIcon";
-import EditIcon from "./EditIcon";
-import EmailIcon from "./EmailIcon";
-import PasswordIcon from "./PasswordIcon";
-
-export { AlternateAvatarImageIcon, EditIcon, PasswordIcon, EmailIcon };
