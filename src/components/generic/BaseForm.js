9e7a0f19695e1f3c47cdf69eb54bcef5efe38c2a
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-03-10 16:56:30 +0700

    Drop garbage

--- src/components/generic/BaseForm.js
+++
@@ -1,9 +0,0 @@
-export default function BaseForm({ autoComplete, onSubmit, children }) {
-    return (
-        <>
-            <form className="m-auto" onSubmit={onSubmit} autoComplete={autoComplete}>
-                {children}
-            </form>
-        </>
-    );
-}
