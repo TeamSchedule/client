157bf53897610203f16269ae5ac3b91713aab635
Yaroslav <yfyl77ov@gmail.com>
2023-03-22 01:45:49 +0700

    new events, styles

--- src/NotificationsTray/NotificationsTray.tsx
+++ src/NotificationsTray/NotificationsTray.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './NotificationsTray.css'
 import { Notification } from '../Models/Notification';
 import { sampleNotifications } from '../SampleNotifications/SampleNotifications';
@@ -12,19 +12,46 @@ function NotificationTray() {
     }, []);
 
     const [showNotifications, setShowNotifications] = useState(false);
+    const notificationRef = useRef<HTMLDivElement>(null);
 
     const handleIconClick = () => {
         setShowNotifications(!showNotifications);
     };
 
+    const handleMarkAllAsRead = () => {
+        const updatedNotifications = notifications.map((notification) => {
+            notification.read = true;
+            return notification;
+        });
+        setNotifications(updatedNotifications);
+    };
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (notificationRef.current && event.target && !notificationRef.current.contains(event.target as Node)) {
+            setShowNotifications(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside, true);
+        return () => {
+            document.removeEventListener('click', handleClickOutside, true);
+        };
+    });
+
     const unreadCount = notifications.filter(notification => !notification.read).length;
 
     return (
-        <div>
-            <span className="notification-count">{unreadCount}</span>
-            <NotificationsIcon onClick={handleIconClick} />
+        <div ref={notificationRef}>
+            <div className="notification-tray-icon" onClick={handleIconClick}>
+                <NotificationsIcon />
+                <span className="notification-count">{unreadCount}</span>
+            </div>
             {showNotifications && (
                 <div className="notification-tray">
+                    <span className="mark-all-as-read-text" onClick={handleMarkAllAsRead}>
+                        Прочитать все
+                    </span>
                     {notifications.map((notification) => (
                         <div
                             key={notification.id}
