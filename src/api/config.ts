ac2c15d2e0c41e7945b255c08e4591a4ed22a673
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-04-02 18:58:00 +0700

    Fix host

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,7 @@
 const ENABLE_HTTPS: boolean = false;
 
-export const SERVER_ORIGIN: string = `http${ENABLE_HTTPS ? "s" : ""}://0.0.0.0`;
+export const SERVER_ORIGIN: string = `http${ENABLE_HTTPS ? "s" : ""}://localhost`;
+// export const SERVER_ORIGIN: string = `http${ENABLE_HTTPS ? "s" : ""}://192.168.0.100`;
 export const AVATARS_STATIC_SERVER: string = "http://localhost:81/avatars";
 export const MEDIA_STATIC_SERVER: string = "http://localhost/media";
 
