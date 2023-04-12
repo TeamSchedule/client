cb994be1fc6a5e3fcf21f1c0d410a88aad3e19ab
Sergey Rubnikovich <sergeyrubnikovich@yandex.ru>
2023-04-12 23:53:48 +0700

    Change store api restriction

--- src/store/utils.ts
+++ src/store/utils.ts
@@ -1,6 +1,7 @@
 import { NonAuthedPaths } from "../routes/paths";
 
-export const isNonAuthenticatedURL: boolean = NonAuthedPaths.some((path) => {
-    console.log(window.location.pathname);
-    return window.location.pathname.startsWith(path);
-});
+export const isNonAuthenticatedURL: boolean =
+    NonAuthedPaths.some((path) => {
+        console.log(window.location.pathname);
+        return window.location.pathname.startsWith(path);
+    }) || window.location.pathname === "/";
